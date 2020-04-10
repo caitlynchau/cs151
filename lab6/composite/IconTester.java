72d4852f383eeb6f5ce574ec91a0995de66628eb
Caitlyn Chau <caitlynchau2@gmail.com>
2020-04-10 16:22:38 -0700

    update path for globe.gif

--- lab6/composite/IconTester.java
+++ lab6/composite/IconTester.java
@@ -9,7 +9,7 @@ public class IconTester {
 	public static void main(String[] args) {
 		CompositeIcon compositeIcon = new CompositeIcon();
 		compositeIcon.addIcon(new MarsIcon(50));
-		compositeIcon.addIcon(new ImageIcon("C:\\Java\\151lab6\\src\\globe.gif"));
+		compositeIcon.addIcon(new ImageIcon("globe.gif"));
 		
 		JOptionPane.showMessageDialog(null, 
 			"Hello, Composite!", 
